d20d7f044ff48db1d84a478f8f9d11b552ed88de
James Gathogo <james@gathogo.co.ke>
2021-06-08 07:32:33 +0300

    problem 3 solution amended

--- week3/problem3.py
+++ week3/problem3.py
@@ -20,9 +20,6 @@ def main():
     print(f"{prod_r0_c0} {prod_r0_c1}\n{prod_r1_c0} {prod_r1_c1}")
 
     # modify program that user can input any matrix
-    m_product = [[prod_r0_c0, prod_r0_c1],
-                 [prod_r1_c0, prod_r1_c1]]
-
     print("Enter values for Matrix 1: ")
     m_1_r_0_c_0 = int(input("Row 0, Column 0: "))
     m_1_r_0_c_1 = int(input("Row 0, Column 1: "))
@@ -35,14 +32,30 @@ def main():
     m_2_r_1_c_0 = int(input("Row 1, Column 0: "))
     m_2_r_1_c_1 = int(input("Row 1, Column 1: "))
 
+    m_1 = [[m_1_r_0_c_0, m_1_r_0_c_1],
+           [m_1_r_1_c_0, m_1_r_1_c_1]]
+
+    print(f"Matrix 1: \n"
+          f"{m_1[0][0]} {m_1[0][1]}\n{m_1[1][0]} {m_1[1][1]}")
+
+    m_2 = [[m_2_r_0_c_0, m_2_r_0_c_1],
+           [m_2_r_1_c_0, m_2_r_1_c_1]]
+
+    print(f"Matrix 2: \n"
+          f"{m_2[0][0]} {m_2[0][1]}\n{m_2[1][0]} {m_2[1][1]}")
+
     prod_r0_c0 = (m_1_r_0_c_0 * m_2_r_0_c_0) + (m_1_r_0_c_1 * m_2_r_1_c_0)
     prod_r0_c1 = (m_1_r_0_c_0 * m_2_r_0_c_1) + (m_1_r_0_c_1 * m_2_r_1_c_1)
     prod_r1_c0 = (m_1_r_1_c_0 * m_2_r_0_c_0) + (m_1_r_1_c_1 * m_2_r_1_c_0)
     prod_r1_c1 = (m_1_r_1_c_0 * m_2_r_0_c_1) + (m_1_r_1_c_1 * m_2_r_1_c_1)
 
+    m_product = [[prod_r0_c0, prod_r0_c1],
+                 [prod_r1_c0, prod_r1_c1]]
+
     # print out resulting matrix
-    # print(m_product, type(m_product))
-    print(f"{prod_r0_c0} {prod_r0_c1}\n{prod_r1_c0} {prod_r1_c1}")
+    print(f"Product of Matrix 1 and 2 is: \n"
+          f"{m_product[0][0]} {m_product[0][1]}\n{m_product[1][0]} {m_product[1][1]}")
+    # print(f"{prod_r0_c0} {prod_r0_c1}\n{prod_r1_c0} {prod_r1_c1}")
 
     return os.EX_OK
 
