4f07ad248dd5a653f6dd706995fb8578717fab6f
James Gathogo <james@gathogo.co.ke>
2021-06-08 10:36:52 +0300

    problem 5 solution

---
+++ week3/problem5.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import random
+
+
+def main():
+    random_list = list(random.choices(range(50, 75), k=100))
+    min_l = min(random_list)
+    max_l = max(random_list)
+
+    l_sorted = sorted(random_list)
+
+    first_letter = l_sorted[0]
+    last_letter = l_sorted[-1]
+
+    print(f"Minimum and the first number are equal: {min_l == first_letter}\n"
+          f"Maximum and the last number are equal: {max_l == last_letter}")
+
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())
