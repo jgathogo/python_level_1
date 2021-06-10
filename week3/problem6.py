dfc2ebf0030f1f2adccd242841dbbb2068da1563
James Gathogo <james@gathogo.co.ke>
2021-06-10 18:24:07 +0300

    variable re-naming for problem 6

--- week3/problem6.py
+++ week3/problem6.py
@@ -42,23 +42,23 @@ def main():
     y = (y1 - y2) ** 2
     z = (z1 - z2) ** 2
 
-    d = math.sqrt(x + y + z)
+    d = round(math.sqrt(x + y + z), 4)
 
     print(f"a. The distance between point A{x1, y1, z1} and B{x2, y2, z2} is: {d}")
 
     # Distance between points and origin
-    x = (0 - x1) ** 2
-    y = (0 - y1) ** 2
-    z = (0 - z1) ** 2
+    m = (0 - x1) ** 2
+    n = (0 - y1) ** 2
+    o = (0 - z1) ** 2
 
-    d_a = math.sqrt(x + y + z)
+    d_a = round(math.sqrt(m + n + o), 4)
     print(f"b.1 . Distance between point A{x1, y1, z1} and origin(0, 0, 0) is: {d_a}")
 
-    x = (0 - x2) ** 2
-    y = (0 - y2) ** 2
-    z = (0 - z2) ** 2
+    r = (0 - x2) ** 2
+    s = (0 - y2) ** 2
+    t = (0 - z2) ** 2
 
-    d_b = math.sqrt(x + y + z)
+    d_b = round(math.sqrt(r + s + t), 4)
     print(f"b.2 . Distance between point B{x2, y2, z2} and origin(0, 0, 0) is: {d_b}")
 
     # Use cosine rule to compute internal angles
@@ -66,21 +66,21 @@ def main():
     b = d_b
     c = d
     cos_d = (a ** 2 + b ** 2 - c ** 2) / (2 * a * b)
-    angle_d = math.degrees(math.acos(cos_d))
+    angle_d = round(math.degrees(math.acos(cos_d)), 4)
     print(f"Angle D, at origin, (in degrees) is: {angle_d}")
 
     a = d
     b = d_b
     c = d_a
     cos_d = (a ** 2 + b ** 2 - c ** 2) / (2 * a * b)
-    angle_da = math.degrees(math.acos(cos_d))
+    angle_da = round(math.degrees(math.acos(cos_d)), 4)
     print(f"Angle A (in degrees) is: {angle_da}")
 
     a = d
     b = d_a
     c = d_b
     cos_d = (a ** 2 + b ** 2 - c ** 2) / (2 * a * b)
-    angle_db = math.degrees(math.acos(cos_d))
+    angle_db = round(math.degrees(math.acos(cos_d)), 4)
     print(f"Angle B (in degrees) is: {angle_db}")
 
     print(f"Check if the three angles add up to 180 degrees: "
