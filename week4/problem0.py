0c8fd1c9b5c1df8eae82b35391eb76829ffa3cd7
James Gathogo <james@gathogo.co.ke>
2021-06-18 14:21:45 +0300

    problem 0 update

--- week4/problem0.py
+++ week4/problem0.py
@@ -10,25 +10,22 @@ Notes/Questions:
     
 - Please make you code more compact; fewer blank lines
 - If the user does not follow instructions the program should halt. 
-How would you implement this?
+How would you implement this? I think sys.exit()
 """
 
 
 def main():
     text = input("Enter a ten character word: ")
-
     if len(text) != 10:
         print("Number of characters should be exactly 10 in number.\n"
               "Please run the program again and enter exactly 10 characters")
-
+        sys.exit()
     d = {}
-
     for i in text:
         if i not in d:
             d[i] = 1
         else:
             d[i] = d[i] + 1
-
     print(d)
     return os.EX_OK
 
