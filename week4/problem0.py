74e240997b1b2c17cb1845740608b20f10d2114a
James Gathogo <james@gathogo.co.ke>
2021-06-23 17:58:22 +0300

    problem 0 update

--- week4/problem0.py
+++ week4/problem0.py
@@ -19,19 +19,13 @@ def main():
     if len(text) != 10:
         print("Number of characters should be exactly 10 in number.\n"
               "Please run the program again and enter exactly 10 characters")
-        # todo: OK. This is OK. However, remember that right at the bottom
-        #  we have sys.exit(main()); all we need to do is return an exit
-        #  status instead of actually exiting; this way we can avoid our
-        #  module exiting at two places
-        # sys.exit()
-        # better is
-        return os.EX_USAGE  # wrong usage is a well-known exit status
+        sys.exit()
     d = {}
     for i in text:
         if i not in d:
             d[i] = 1
         else:
-            d[i] = d[i] + 1
+            d[i] += 1
     print(d)
     return os.EX_OK
 
