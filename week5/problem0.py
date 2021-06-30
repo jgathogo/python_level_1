aced300448ae93da257e3b42fb2f716cc499a009
James Gathogo <james@gathogo.co.ke>
2021-06-30 18:18:49 +0300

    problem 0 solution

--- week5/problem0.py
+++ week5/problem0.py
@@ -10,35 +10,30 @@ def main():
 
     print(f"Cumulative sum: ")
     new_l = []
-    i = 0
-    for num in l:
+    for i in range(len(l)):
         new_l.append(sum(l[0:i + 1]))
-        i += 1
     print(new_l)
 
     # New list of the sum of adjoining pairs
     print(f"Sum of adjoining pairs: ")
     new_l2 = []
-    i = 0
-    for num in l:
+    for i in range(len(l)):
         pos1 = i
         pos2 = pos1 + 1
         if pos1 < len(l) and pos2 < len(l):
             new_l2.append(l[pos1] + l[pos2])
-            i += 1
     print(new_l2)
 
     # New list of the sum of 3 alternate values
     print(f"Sum of 3 alternate values")
     new_l3 = []
     i = 0
-    for num in l:
+    for i in range(len(l)):
         pos1 = i
         pos2 = pos1 + 2
         pos3 = pos2 + 2
         if pos1 < len(l) and pos2 < len(l) and pos3 < len(l):
             new_l3.append(l[pos1] + l[pos2] + l[pos3])
-        i += 1
     print(new_l3)
 
     return os.EX_OK
