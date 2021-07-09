b5af7e8e10818279f59e31f3ebd6e06974161f91
James Gathogo <james@gathogo.co.ke>
2021-07-09 12:13:22 +0300

    problem 4 solution

---
+++ week6/problem4.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import random
+
+
+def main():
+    f_l = []
+    for num in range(15):
+        f_l.append(round(random.uniform(1, 10), 2))
+    print(f"List of floats: {f_l}")
+    double = list(map(lambda x: round(x ** 2, 2), f_l))
+    print(f"Doubles: {double}")
+    return os.EX_OK
+
+
+if __name__ == "__main__":
+    sys.exit(main())
